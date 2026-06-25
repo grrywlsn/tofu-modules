4b4c67ce74f8f98d2f33713143e3615628ba8861
grrywlsn <6753180+grrywlsn@users.noreply.github.com>
2026-06-25 12:02:40 +0000

    feat(scaleway-network): update terraform scaleway to 2.77.0

--- scaleway-network/versions.tf
+++ scaleway-network/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "2.75.0"
+      version = "2.77.0"
     }
   }
   required_version = ">= 1.4"
