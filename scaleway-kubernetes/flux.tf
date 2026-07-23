640089b136f139b9c8dae34a3cbd9b854075737e
grrywlsn <6753180+grrywlsn@users.noreply.github.com>
2026-07-23 16:42:16 +0000

    feat(scaleway-kubernetes): update flux-instance docker tag to v0.56.0

--- scaleway-kubernetes/flux.tf
+++ scaleway-kubernetes/flux.tf
@@ -34,7 +34,7 @@ resource "helm_release" "flux_instance" {
   namespace  = "flux-system"
   repository = "oci://ghcr.io/controlplaneio-fluxcd/charts"
   chart      = "flux-instance"
-  version    = "0.55.0"
+  version    = "0.56.0"
 
   values = [
     file("values/components.yaml"),
