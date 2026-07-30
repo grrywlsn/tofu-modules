1692396b42cd902070816650d3291727bd0043fa
grrywlsn <6753180+grrywlsn@users.noreply.github.com>
2026-07-30 09:42:15 +0000

    feat(bunnynet-dns-zone): update terraform bunnynet to 0.16.0

--- bunnynet-dns-zone/versions.tf
+++ bunnynet-dns-zone/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     bunnynet = {
       source  = "BunnyWay/bunnynet"
-      version = "0.15.2"
+      version = "0.16.0"
     }
   }
   required_version = ">= 1.3"
