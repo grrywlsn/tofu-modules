b9a9c26b092e6993d1e22e5799960ac920fcc540
grrywlsn <6753180+grrywlsn@users.noreply.github.com>
2026-08-21 09:02:33 +0000

    fix(bunnynet-dns-zone): update terraform bunnynet to 0.18.1

--- bunnynet-dns-zone/versions.tf
+++ bunnynet-dns-zone/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     bunnynet = {
       source  = "BunnyWay/bunnynet"
-      version = "0.18.0"
+      version = "0.18.1"
     }
   }
   required_version = ">= 1.3"
